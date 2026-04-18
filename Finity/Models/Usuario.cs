58d0434948f035eec1df1cb1ee8e1e1d29700d4d
ReinaldoBarbosa <reinaldobarbosa034@gmail.com>
2026-04-18 01:13:34 -0300

    Create UserModel

---
+++ Finity/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿namespace Finity.Models
+{
+    public class Usuario
+    {
+        private int id { get; set; }
+        private String nome {  get; set; }
+        private String email { get; set; }
+        private String senha { get; set; }
+        private String cargo { get; set; }
+        private String nivel_acesso { get; set; }
+        private int id_departamento { get; set; }
+        private String status { get; set; }
+        private String matricula { get; set; }
+    }
+}
